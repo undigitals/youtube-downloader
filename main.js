c3cfe8c7b1e1e8980521555cded692ebb59a282e
Umid Negmatullayev <undigitals@gmail.com>
2018-11-12 07:38:15 +0900

    commit

---
+++ main.js
@@ -0,0 +1,121 @@
+const {app, BrowserWindow, Menu} = require('electron');
+const isDevMode = require('electron-is-dev');
+const path = require('path');
+
+if (isDevMode) {
+  require('electron-reload')(__dirname + '/public');
+}
+
+let mainWindow;
+
+
+function createWindow() {
+  const browserOptions = {
+    width: 800,
+    height: 600,
+    maximizeable: false,
+    icon: path.join(__dirname, '/public/img/logo.png')
+  };
+
+  
+  mainWindow = new BrowserWindow(browserOptions);
+  mainWindow.loadURL('file://' + __dirname + '/app/index.html');
+  
+  var deleteChromeCache = function() {
+      var chromeCacheDir = path.join(app.getPath('userData'), 'Cache'); 
+      if(fs.existsSync(chromeCacheDir)) {
+          var files = fs.readdirSync(chromeCacheDir);
+          for(var i=0; i<files.length; i++) {
+              var filename = path.join(chromeCacheDir, files[i]);
+              if(fs.existsSync(filename)) {
+                  try {
+                      fs.unlinkSync(filename);
+                  }
+                  catch(e) {
+                      console.log(e);
+                  }
+              }
+          }
+      }
+  };
+
+  mainWindow.webContents.clearHistory();  
+
+  let template = [{
+    label: 'YouTube Download',
+    submenu: [
+      {label: 'About Application', selector: 'orderFrontStandardAboutPanel:'},
+      {type: 'separator'},
+      {
+        label: 'Quit', accelerator: 'Command+Q', click: function () {
+          app.quit();
+        }
+      }
+    ]
+  }, {
+    label: 'Edit',
+    submenu: [
+      {label: 'Undo', accelerator: 'CmdOrCtrl+Z', selector: 'undo:'},
+      {label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', selector: 'redo:'},
+      {type: 'separator'},
+      { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" },
+      {label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:'},
+      {label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:'},
+      {label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:'}
+    ]
+  }, {
+    label: 'Preferences',
+    submenu: [{
+        label: 'Download Folder',
+        click: () => {
+          mainWindow.webContents.send('promptForChangeDownloadFolder');
+        }
+      },
+      {
+        label: 'Change Bitrate',
+        submenu: [
+          {label: '320 (Full Quality)', click: () => {mainWindow.webContents.send('changeBitrate', 320)}},
+          {label: '192 (High Quality)', click: () => {mainWindow.webContents.send('changeBitrate', 192)}},
+          {label: '160 (CD Quality)', click: () => {mainWindow.webContents.send('changeBitrate', 160)}},
+          {label: '130 (Radio Quality)', click: () => {mainWindow.webContents.send('changeBitrate', 130)}},
+          {label: '65 (Minimal Quality)', click: () => {mainWindow.webContents.send('changeBitrate', 65)}},
+        ]
+      }
+    ]
+  }
+  ];
+
+  // If developing add dev menu option to menu bar
+  if (isDevMode) {
+    template.push({
+      label: 'Dev Options',
+      submenu: [
+        {
+          label: 'Open Dev Tools', click: () => {
+            mainWindow.webContents.openDevTools()
+          }
+        }
+      ]
+    });
+  }
+
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
